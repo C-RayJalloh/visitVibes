1bbdb66a6b9ad73212821aab634e16635481483c
“Jahrulo” <mserayjay@gmail.com>
2024-02-25 14:56:56 +0000

    updated

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const dbUrl =  process.env.DB_URL ||  'mongodb://localhost:27017/placeFinder';
 // Additional SSL/TLS options if your MongoDB server uses SSL
 const sslOptions = {
     ssl: true,
-    sslValidate: true,
+    tlsAllowInvalidCertificates: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
